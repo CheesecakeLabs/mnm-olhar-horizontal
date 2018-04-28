6294abbfd04245949d4c011d3d7af3c4b6069be0
FL-SS-204-10\Aluno <djonys2002@hotmail.com>
2018-04-28 11:32:27 -0300

    Create LoginActivity and Layout activty_login

---
+++ app/src/main/java/br/com/maonamassa/olharhorizontal/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package br.com.maonamassa.olharhorizontal.activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import br.com.maonamassa.olharhorizontal.R
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
