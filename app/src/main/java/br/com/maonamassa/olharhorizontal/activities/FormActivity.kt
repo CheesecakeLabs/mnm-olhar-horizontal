dd3f8cef3b77b917e450fc8e62167b6eac188e0b
GehanC <gehanlag01@gmail.com>
2018-04-28 11:28:30 -0300

    Criação do FormActivity.xml para inserir as informações do grupo Form.

---
+++ app/src/main/java/br/com/maonamassa/olharhorizontal/activities/FormActivity.kt
@@ -0,0 +1,17 @@
+package br.com.maonamassa.olharhorizontal.activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import br.com.maonamassa.olharhorizontal.R
+
+/**
+ * Created by Aluno on 28/04/2018.
+ */
+class FormActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
