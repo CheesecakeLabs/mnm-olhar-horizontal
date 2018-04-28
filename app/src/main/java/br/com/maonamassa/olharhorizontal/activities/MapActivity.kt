9d709c6cdf58b0d728ebe077e033854a1920a9d5
FL-SS-204-15\Aluno <eduardosiqueira26@gmail.com>
2018-04-28 11:26:43 -0300

    Create activity for the map

---
+++ app/src/main/java/br/com/maonamassa/olharhorizontal/activities/MapActivity.kt
@@ -0,0 +1,17 @@
+package br.com.maonamassa.olharhorizontal.activities
+
+/**
+ * Created by Eduardo on 28/04/2018.
+ */
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.maonamassa.olharhorizontal.R
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreateContextMenu(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
