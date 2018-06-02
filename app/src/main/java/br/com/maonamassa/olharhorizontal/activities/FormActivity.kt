c39fb0a21e3cde3478987b92532eb3e9f3078630
FL-SS-204-21\Aluno <tyududy3@gmail.com>
2018-06-02 12:10:17 -0300

    Criacao da tela de atividades

--- app/src/main/java/br/com/maonamassa/olharhorizontal/activities/FormActivity.kt
+++ app/src/main/java/br/com/maonamassa/olharhorizontal/activities/FormActivity.kt
@@ -10,7 +10,7 @@ import br.com.maonamassa.olharhorizontal.R
  */
 class FormActivity: AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
     }
