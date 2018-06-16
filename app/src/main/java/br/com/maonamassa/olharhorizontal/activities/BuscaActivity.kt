48a20af63be66d9d1e58bb5926b85ef08706a630
aluno <darkwarish@gmail.com>
2018-06-16 12:13:36 -0300

    Adiciona activity de busca

---
+++ app/src/main/java/br/com/maonamassa/olharhorizontal/activities/BuscaActivity.kt
@@ -0,0 +1,18 @@
+package br.com.maonamassa.olharhorizontal.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import br.com.maonamassa.olharhorizontal.R
+
+/**
+ * Created by Aluno on 16/06/2018.
+ */
+class BuscaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_busca)
+    }
+}
