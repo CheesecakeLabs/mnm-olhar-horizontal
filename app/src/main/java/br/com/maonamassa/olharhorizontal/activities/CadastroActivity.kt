fa1b602fab8ee16042b49a0a9aec0bcef6677e7e
Mateus Oliveira <mts_ods@hotmail.com>
2018-06-16 12:05:14 -0300

    Adds CadastroActivity, links LoginActivity to CadastroActivity and updates layout

---
+++ app/src/main/java/br/com/maonamassa/olharhorizontal/activities/CadastroActivity.kt
@@ -0,0 +1,15 @@
+package br.com.maonamassa.olharhorizontal.activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import br.com.maonamassa.olharhorizontal.R
+/**
+ * Created by Aluno on 16/06/2018.
+ */
+class CadastroActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
--- app/src/main/java/br/com/maonamassa/olharhorizontal/activities/LoginActivity.kt
+++ app/src/main/java/br/com/maonamassa/olharhorizontal/activities/LoginActivity.kt
@@ -1,13 +1,20 @@
 package br.com.maonamassa.olharhorizontal.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import br.com.maonamassa.olharhorizontal.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        botaoCadastrar.setOnClickListener {
+            var intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
