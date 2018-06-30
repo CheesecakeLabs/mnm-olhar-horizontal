34bf635c84afaee686f01f8eba25be3d3c242bd8
19Mateus19 <mts_ods@hotmail.com>
2018-06-30 10:22:22 -0300

    Implement back button in "CadastroActivity"

--- app/src/main/java/br/com/maonamassa/olharhorizontal/activities/CadastroActivity.kt
+++ app/src/main/java/br/com/maonamassa/olharhorizontal/activities/CadastroActivity.kt
@@ -3,6 +3,7 @@ package br.com.maonamassa.olharhorizontal.activities
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import android.view.View
 import br.com.maonamassa.olharhorizontal.R
 import kotlinx.android.synthetic.main.activity_cadastro.*
@@ -30,5 +31,18 @@ class CadastroActivity : AppCompatActivity() {
                 localizacaotextimput.visibility = View.GONE
             }
         }
+
+        title = "Cadastro"
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if (item?.itemId == android.R.id.home){
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
