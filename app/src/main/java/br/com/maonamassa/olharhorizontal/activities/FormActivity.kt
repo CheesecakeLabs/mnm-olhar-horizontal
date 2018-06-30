096c2bc33607c39b8fecadcb7fb64d3f6ffe95c3
Endy Silveira <endy.silveira@gmail.com>
2018-06-30 10:18:10 -0300

    Adicionar RxPermissions lib e a variavel no FormActivity

--- app/src/main/java/br/com/maonamassa/olharhorizontal/activities/FormActivity.kt
+++ app/src/main/java/br/com/maonamassa/olharhorizontal/activities/FormActivity.kt
@@ -1,21 +1,19 @@
 package br.com.maonamassa.olharhorizontal.activities
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.TextView
 import br.com.maonamassa.olharhorizontal.R
 import br.com.maonamassa.olharhorizontal.modelos.ONG
 import br.com.maonamassa.olharhorizontal.modelos.Organizacao
-import br.com.maonamassa.olharhorizontal.utils.OngAdapter
 import br.com.maonamassa.olharhorizontal.utils.ProjetosApi
 import br.com.maonamassa.olharhorizontal.utils.RetrofitHelper
+import com.tbruyelle.rxpermissions2.RxPermissions
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_form.*
-import kotlinx.android.synthetic.main.activity_lista.*
 import java.util.*
 
 /**
@@ -29,9 +27,10 @@ class FormActivity: AppCompatActivity(), DatePickerDialog.OnDateSetListener {
         setupView()
     }
 
+    fun setupView() {
 
+        val rxPermissions = RxPermissions(this)
 
-    fun setupView () {
         dataInput.setOnClickListener {
             val now = Calendar.getInstance()
             val dpd = DatePickerDialog.newInstance(
@@ -45,28 +44,28 @@ class FormActivity: AppCompatActivity(), DatePickerDialog.OnDateSetListener {
             dpd.show(fragmentManager, "Datepickerdialog")
         }
 
-        sendImage.setOnClickListener () {
+        sendImage.setOnClickListener() {
             //Implementar funcionalidade
 
         }
 
         sendButton.setOnClickListener() {
-            var ong=ONG ()
+            var ong = ONG()
 
-            val name= nameEditText.text.toString()
-            ong.nome= name
+            val name = nameEditText.text.toString()
+            ong.nome = name
 
             //val date=dataInput.text.toString()
             //ong.dataProjeto= date
 
-            val descricption=descricaoEdit.text.toString()
-            ong.descricao= descricption
+            val descricption = descricaoEdit.text.toString()
+            ong.descricao = descricption
 
-            val time=textHour.text.toString()
-            ong.horario= time
+            val time = textHour.text.toString()
+            ong.horario = time
 
-            val address= enderecoEd.text.toString ()
-            ong.endereco= address
+            val address = enderecoEd.text.toString()
+            ong.endereco = address
 
             //val image= sendImage.text.toString ()
             //ong.fotoUrl= image
@@ -91,19 +90,18 @@ class FormActivity: AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
             createProject(ong)
 
-
         }
 
         textHour.setOnClickListener() {
             val now = Calendar.getInstance()
-            val dpd = TimePickerDialog.newInstance(object: TimePickerDialog.OnTimeSetListener{
+            val dpd = TimePickerDialog.newInstance(object: TimePickerDialog.OnTimeSetListener {
                 override fun onTimeSet(dialog: TimePickerDialog?, hora: Int, minuto: Int, segundo: Int) {
-                    var textH= "$hora:$minuto"
+                    var textH = "$hora:$minuto"
                     textHour.setText(textH, TextView.BufferType.EDITABLE)
-                   //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 }
 
-            }, true )
+            }, true)
             dpd.show(fragmentManager, "Datepickerdialog")
         }
 
@@ -118,8 +116,8 @@ class FormActivity: AppCompatActivity(), DatePickerDialog.OnDateSetListener {
         val retrofit = RetrofitHelper.getRetrofit(useAuth = false)
         val apiInterface = retrofit.create(ProjetosApi::class.java)
         apiInterface?.createProject(ong)?.subscribeOn(Schedulers.io())?.subscribe({
-                    Log.e( "sucesso!!", "Objeto enviado com sucesso")
-                }, { error ->
+            Log.e("sucesso!!", "Objeto enviado com sucesso")
+        }, { error ->
             Log.e("Erro", "Deu ruim na API")
         }
         )
