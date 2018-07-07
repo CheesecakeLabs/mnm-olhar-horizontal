283404edae56670c4245a5b88122b631a6ce991f
Daniel Rosa <darkwarish@gmail.com>
2018-07-07 09:59:37 -0300

    Criação da Classe ViewHolder de Busca.

---
+++ app/src/main/java/br/com/maonamassa/olharhorizontal/utils/OngBuscaViewHolder.kt
@@ -0,0 +1,11 @@
+package br.com.maonamassa.olharhorizontal.utils
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import br.com.maonamassa.olharhorizontal.R
+
+class OngBuscaViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_ong_busca, parent, false)) {
+
+}
